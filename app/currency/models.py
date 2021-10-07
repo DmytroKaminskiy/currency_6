8dde746d3946860c1cf859ae29f870ebd86633bb
dmytro.kaminskyi92@gmail.com <dmytro.kaminskyi92@gmail.com>
2021-10-07 21:08:03 +0300

    hw6

--- app/currency/models.py
+++ app/currency/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Rate(models.Model):
-    buy = models.DecimalField(max_digits=6, decimal_places=2)
-    sale = models.DecimalField(max_digits=6, decimal_places=2)
+    buy=models.DecimalField(max_digits=6, decimal_places=2)
+    sale =models.DecimalField(max_digits=6, decimal_places=2)
     created = models.DateTimeField()
     type = models.CharField(max_length=3)  # noqa: A003
     source = models.CharField(max_length=25)
